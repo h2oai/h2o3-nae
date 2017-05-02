fe74934febdda7e8df940b0b9f4e98a646c2acdf
ops <ops@mr-0xc8.0xdata.loc>
2017-05-02 12:59:52 -0700

    change logic to make flatfile in start-cluster itself

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cat nodes | while read n;
+cp /etc/JARVICE/nodes /opt/flatfile.txt
+sed -e 's/$/:54321/' -i /opt/flatfile.txt
+
+cat /etc/JARVICE/nodes | while read n;
 do
   echo "Starting node $n"
   scp flatfile.txt nimbix@$n:/opt/flatfile.txt
