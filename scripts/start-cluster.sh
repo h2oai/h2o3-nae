5d8bd6eb5f700b73563d8ad76cc59bc86e8ac591
ops <ops@mr-0xc8.0xdata.loc>
2017-05-03 12:51:04 -0700

    wrap in sudo

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Change Nginx Redirect
-sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/default
-sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/notebook-site
-/etc/init.d/nginx restart
+sudo sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/default
+sudo sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/notebook-site
+sudo /etc/init.d/nginx restart
 
 cat /etc/JARVICE/nodes | while read n;
 do
@@ -12,13 +12,8 @@ do
   /opt/sssh nimbix@$n /opt/start-h2o3.sh &
 done
 
-# Change Nginx Redirect
-sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/default
-sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/notebook-site
-
 # Start Notebook
 /usr/local/bin/nimbix_notebook
 
-
 # Start RStudio
-/etc/init.d/rstudio-server restart
+sudo /etc/init.d/rstudio-server restart
