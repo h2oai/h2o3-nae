a6cbf73f0ce356f3e51540de0c50410d8e0af599
ops <ops@mr-0xc8.0xdata.loc>
2017-05-09 00:38:27 -0700

    add start-rstudio.sh

---
+++ scripts/start-rstudio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change Nginx Redirect
+sudo sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/default
+sudo sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/notebook-site
+
+# Start SSH
+sudo system ssh restart
+# Start RStudio
+sudo service nginx restart
+
+sudo /etc/init.d/rstudio-server restart
