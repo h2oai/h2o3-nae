2a195178ad0728e3f4e93cd729318d4f36692018
ops <ops@mr-0xc8.0xdata.loc>
2017-05-09 00:49:21 -0700

    force port on rstudio

--- scripts/start-rstudio.sh
+++ scripts/start-rstudio.sh
@@ -4,6 +4,9 @@
 sudo sed -e 's/8888/8787/' -i /etc/nginx/sites-enabled/default
 sudo sed -e 's/8888/8787/' -i /etc/nginx/sites-enabled/notebook-site
 
+rm -f /etc/NAE/url.txt
+echo "http://%PUBLICADDR%:8787/" > /etc/NAE/url.txt
+
 # Start SSH
 sudo system ssh restart
 # Start RStudio
